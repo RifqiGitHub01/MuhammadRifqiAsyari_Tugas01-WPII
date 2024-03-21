9b27c23facf4cc988b1000dcd83defb718869c90
RifqiGitHub01 <rifqi6886@gmail.com>
2024-03-21 14:13:36 +0700

    Tugas 01 WPII

---
+++ app/Controllers/Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Muhammad Rifqi Asy'ari <br>
+        Saya tinggal di daerah Condet <br>
+        Olahraga yang saya sukai adalah badminton";
+    }
+}
